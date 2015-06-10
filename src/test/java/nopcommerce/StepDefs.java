dc9ff96f82051ccb1dcf1b6b84976415884afbc1
Taranjeet1 <tsr6791@gmail.com>
2015-06-10 15:31:37 +0100

    hi

--- src/test/java/nopcommerce/StepDefs.java
+++ src/test/java/nopcommerce/StepDefs.java
@@ -1,8 +1,14 @@
 package nopcommerce;
 import cucumber.annotation.Before;
 import cucumber.annotation.After;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.runtime.PendingException;
+import cucumber.table.DataTable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,28 +17,29 @@ import java.util.concurrent.TimeUnit;
 
 public class StepDefs {
 
-    WebDriver driver;
-   // String url=("http://demo.nopcommerce.com/");
-/*@Before
-public void startBrowser(){
-    driver = new FirefoxDriver();
-    driver.get("http://demo.nopcommerce.com");
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
-    }
-    
-*/
+    static  WebDriver driver;
     @Before
     public void openBrowser(){
         driver=new FirefoxDriver();
-        driver.get("http://demo.nopcommerce.com/");
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.get("http://demo.nopcommerce.com");
+    }
+    @Given("^User is on the Home page$")
+    public void User_is_on_the_Home_page() {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
     }
 
-    @After
-    public void stopBrowser(){
+    @Then("^User should see Categories on  the left panel of the  Home page$")
+    public void User_should_see_Categories_on_the_left_panel_of_the_Home_page() {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
 
-        driver.quit();
+    @Then("^User should be able to Navigate to those Categories page$")
+    public void User_should_be_able_to_Navigate_to_those_Categories_page(DataTable arg1) {
+        // Express the Regexp above with the code you wish you had
+        // For automatic conversion, change DataTable to List<YourType>
+        throw new PendingException();
+    }
 
-                }
 }
